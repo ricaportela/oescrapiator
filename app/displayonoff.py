7e769533ffb7da34adbca0ab5c14ec351af1e783
ricaportela <ricaportela@gmail.com>
2017-03-09 22:09:38 -0300

    Fix Display class

--- app/displayonoff.py
+++ app/displayonoff.py
@@ -5,15 +5,17 @@ from pyvirtualdisplay import Display
 class DisplayOnOff():
     """Display on/off Browser."""
 
+    display = ''
+
     def __init__(self, display):
         """Contructor."""
         self.display = display
 
     def open_virtual_display():
         """Turn of browser display."""
-        self.display = Display(visible=1, size=(100, 100))
-        self.display.start()
+        display = Display(visible=0, size=(100, 100))
+        display.start()
 
     def stop_virtual_display():
         """Set display normal."""
-        self.display.stop()
+        display.stop()
